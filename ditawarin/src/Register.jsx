56e5a435586b9891eef04d894980da0d527b93fb
ElaineBrax <107904402+ElaineBrax@users.noreply.github.com>
2023-11-20 20:49:20 +0700

    Update Register.jsx

--- ditawarin/src/Register.jsx
+++ ditawarin/src/Register.jsx
@@ -1,39 +1,46 @@
+import { useForm } from 'react-hook-form'
 import flag from './assets/flag.svg'
+import { Link } from 'react-router-dom'
 
 function Register() {
+
+    const {register, handleSubmit, reset} = useForm()
+
+    async function signUp(data){
+        
+    }
+
     return(
         <>
             <div className="container">
                 <h1 className="mb-4 text-center">Registration</h1>
-                <form>
+                <form onSubmit={handleSubmit(signUp)}>
                     <div className="row">
                         <div className="col-6">
                             <label>Nama Lengkap </label><br />
-                            <input type="text" name="namalengkap" placeholder="Nama Lengkap" className="mb-3"/> <br />
+                            <input type="text" {...register("namalengkap")} placeholder="Nama Lengkap" className="mb-3"/> <br />
 
                             <label>No. Handphone </label><br />
                             <input type="image" src={flag} alt="" style={{width: "10%", height: "10%"}}/>
-                            <input type="text" name="nohp" placeholder="No. Handphone" className="mb-3"/> <br />
+                            <input type="text" {...register("nohp")} placeholder="No. Handphone" className="mb-3"/> <br />
 
                             <label>Password </label><br />
-                            <input type="text" name="pass" placeholder="Password" className="mb-3"/> <br />
+                            <input type="text" {...register("pass")} placeholder="Password" className="mb-3"/> <br />
                         </div>
                         <div className="col-6">
                             <label>Email </label><br />
-                            <input type="text" name="email" placeholder="Email" className="mb-3"/> <br />
+                            <input type="text" {...register("email")} placeholder="Email" className="mb-3"/> <br />
 
                             <label>Kota </label><br />
-                            <input type="text" name="kota" placeholder="Kota" className="mb-3"/> <br />
+                            <input type="text" {...register("kota")} placeholder="Kota" className="mb-3"/> <br />
 
                             <label>Konfirmasi Password </label><br />
-                            <input type="text" name="confpass" placeholder="confpass" className="mb-3"/>
+                            <input type="text" {...register("confpass")} placeholder="confpass" className="mb-3"/>
                         </div>
                     </div>
-
-                    
-                    
-                    <button>Daftar</button>
+                    <button type='submit'>Daftar</button>
                 </form>
+                <div>Sudah punya akun?  <Link to={"/login"}>Masuk Sini</Link> </div>
             </div>
         </>
     )
