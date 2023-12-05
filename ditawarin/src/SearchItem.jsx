b43d948dbbf53df97dfd5b2849a73f64db312aeb
gaby <76735916+gabypaulina@users.noreply.github.com>
2023-12-06 00:45:49 +0700

    update overflow n navbar

--- ditawarin/src/SearchItem.jsx
+++ ditawarin/src/SearchItem.jsx
@@ -1,55 +1,80 @@
-import plus from "./assets/plus.png"
-import location from './assets/loc.png'
+import plus from "./assets/plus.png";
+import location from "./assets/loc.png";
 import { useNavigate } from "react-router-dom";
-export default function SearchItem(props){
-    console.log(props)
+export default function SearchItem(props) {
+  console.log(props);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    let Rupiah = new Intl.NumberFormat("id-ID", {
-        style: "currency",
-        currency: "IDR"
-    });
+  let Rupiah = new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+  });
 
-    const moveToAuction = () => {
-        navigate('/listing/'+props.auction._id);
-    }
+  const moveToAuction = () => {
+    navigate("/listing/" + props.auction._id);
+  };
 
-    const url = import.meta.env.VITE_API_URL+'/static/'+props.item.images || "";
+  const url =
+    import.meta.env.VITE_API_URL + "/static/" + props.item.images || "";
 
-    return (
-        <>
-        <div className="container" onClick={()=>{moveToAuction()}}>
-            <div className="card mb-3 mt-4" style={{height: "30vh", width: "70vw"}}>
-                <div className="row g-0">
-                    <div className="col-md-4 text-center">
-                        <img src={url} className="img-fluid rounded-start" style={{height: "30vh"}}/>
-                    </div>
-                    <div className="col-md-8">
-                    <div className="card-body" >
-                        <p className="card-title" style={{fontSize: "35px"}}>{props.item.nama}</p>
-                        <div className="row mb-3">
-                            <div className="col-6">
-                                <p className="card-text" style={{fontSize: "20px"}}>
-                                    Starting at : {Rupiah.format(props.auction.starting_price)}
-                                </p>
-                                <p className="card-text" style={{fontSize: "20px"}}>
-                                    Buy it now : {Rupiah.format(props.auction.asking_price)}
-                                </p>
-                            </div>
-                            <div className="col-6">
-                                <img src={location} alt="" style={{width: "20px"}} className="ms-2"/>
-                                {props.auction.kecamatan},{props.auction.kota_kabupaten},{props.auction.provinsi}
-                            </div>
-                        </div>
-                        <p className="card-text overflow-hidden h-25" style={{fontSize: "15px", height:"50%", overflowY:"hidden"}}>
-                            {props.item.deskripsi}
-                        </p>
-                    </div>
-                    </div>
-                </div>
+  return (
+    <>
+      <div
+        className="container"
+        onClick={() => {
+          moveToAuction();
+        }}
+      >
+        <div
+          className="card mb-3 mt-4"
+          style={{ minHeight: "30vh", width: "60vw" }}
+        >
+          <div className="row g-0">
+            <div className="col-md-4 text-center">
+              <img
+                src={url}
+                className="img-fluid rounded-start"
+                style={{ height: "30vh" }}
+              />
             </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <p className="card-title" style={{ fontSize: "35px" }}>
+                  {props.item.nama}
+                </p>
+                <div className="row mb-3">
+                  <div className="col-6">
+                    <p className="card-text" style={{ fontSize: "20px" }}>
+                      Starting at :{" "}
+                      {Rupiah.format(props.auction.starting_price)}
+                    </p>
+                    <p className="card-text" style={{ fontSize: "20px" }}>
+                      Buy it now : {Rupiah.format(props.auction.asking_price)}
+                    </p>
+                  </div>
+                  <div className="col-6">
+                    <img
+                      src={location}
+                      alt=""
+                      style={{ width: "20px" }}
+                      className="ms-2"
+                    />
+                    {props.auction.kecamatan},{props.auction.kota_kabupaten},
+                    {props.auction.provinsi}
+                  </div>
+                </div>
+                <p
+                  className="card-text overflow-y-auto"
+                  style={{ fontSize: "15px", maxHeight: "80px" }}
+                >
+                  {props.item.deskripsi}
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
--- ditawarin/src/components/Navbar.jsx
+++ ditawarin/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
-import { Outlet, useNavigate } from "react-router-dom"
-import Logo from '../assets/logo.png'
+import { Outlet, useNavigate } from "react-router-dom";
+import Logo from "../assets/logo.png";
 import { Link, NavLink } from "react-router-dom";
 import client from "../client";
 import { useEffect } from "react";
@@ -8,63 +8,122 @@ import { useLocation } from "react-router-dom";
 import Footer from "./Footer";
 import { useLoaderData } from "react-router-dom";
 
-export default function Navbar(){
-    let userToken = localStorage.getItem("token");
+export default function Navbar() {
+  let userToken = localStorage.getItem("token");
 
-    let Rupiah = new Intl.NumberFormat("id-ID", {
-        style: "currency",
-        currency: "IDR"
-    });
+  let Rupiah = new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+  });
 
-    const data = useLoaderData();
+  const data = useLoaderData();
 
-    console.log(data);
+  console.log(data);
 
-    const location = useLocation()
-    const navigate = useNavigate()
+  const location = useLocation();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        userToken = localStorage.getItem("token");
-    }, [location]);
+  useEffect(() => {
+    userToken = localStorage.getItem("token");
+  }, [location]);
 
-    function handleKeyPress(e) {
-        if (e.key === 'Enter') {
-            navigate('/search/'+e.target.value);
-        }
+  function handleKeyPress(e) {
+    if (e.key === "Enter") {
+      navigate("/search/" + e.target.value);
     }
+  }
 
-    const url_pp = import.meta.env.VITE_API_URL+'/static/'+data.profile_picture || "";
+  const url_pp =
+    import.meta.env.VITE_API_URL + "/static/" + data.profile_picture || "";
 
-    return (
-        <>
-            <nav className="p-0" style={{borderBottom: "1px solid gray"}}>
-                <div className="container-fluid">
-                    <div className="d-flex justify-content-between align-items-center">
-                        <Link to="/"><img src={Logo} alt="" style={{width: "15%", height: "15%"}}/></Link>
-                        <input type="text" placeholder='Cari Produk' onKeyUp={handleKeyPress.bind(this)} className="border border-secondary-subtle ps-3 me-3" style={{borderRadius: "8px", width: "40%", height: "2rem"}}/>
-                        <div className="d-flex align-items-center">
-                        {!userToken && 
-                            <>
-                                <NavLink type="button" className="btn btn-outline-primary me-3" to="/login">Masuk</NavLink>
-                                <NavLink type="button" className="btn btn-primary" to="/register">Daftar</NavLink>
-                            </>
-                        }
-                        {userToken &&
-                            <>
-                                <img style={{height:"4vh"}} className="nav-item rounded-circle" alt="avatar_user" src={url_pp} />
-                                <NavLink className="nav-item" to="/profile"><p className="mb-0">Hi, {data.nama||"User"}</p></NavLink>
-                                <NavLink className="nav-item" to="/wallet"><p className="mb-0">Saldo: {Rupiah.format(parseInt(data.wallet))}</p></NavLink>
-                                <NavLink type="button" className="btn btn-primary ms-3 me-3" to="/sell">Jual</NavLink>
-                                {/* <NavLink type="button" className="btn btn-primary" to="/profile">Profile</NavLink> */}
-                                <NavLink type="button" className="btn btn-primary" to="/logout" onClick={()=>handleClick()}>Logout</NavLink>
-                            </>
-                        }
-                    </div>
-                 </div>
-                </div>
-            </nav>
-            <Outlet/>
-            <Footer/>
-        </>
-    )
+  return (
+    <>
+      <nav className="p-0" style={{ borderBottom: "1px solid gray" }}>
+        <div className="container-fluid" style={{ backgroundColor: "#06083D" }}>
+          <div className="d-flex align-items-center">
+            <Link to="/" style={{ width: "80px" }}>
+              <img src={Logo} alt="" className="img-fluid" />
+            </Link>
+            <input
+              type="text"
+              placeholder="Cari Produk"
+              onKeyUp={handleKeyPress.bind(this)}
+              className="border border-secondary-subtle p-4 mx-auto"
+              style={{ borderRadius: "8px", width: "40%", height: "2rem" }}
+            />
+            <div className="d-flex align-items-center">
+              {!userToken && (
+                <>
+                  <NavLink
+                    type="button"
+                    className="btn btn-outline-primary me-3"
+                    to="/login"
+                  >
+                    Masuk
+                  </NavLink>
+                  <NavLink
+                    type="button"
+                    className="btn btn-primary"
+                    to="/register"
+                  >
+                    Daftar
+                  </NavLink>
+                </>
+              )}
+              {userToken && (
+                <>
+                  <img
+                    style={{ height: "48px" }}
+                    className="nav-item rounded-circle me-2"
+                    alt="avatar_user"
+                    src={url_pp}
+                  />
+                  <div className="me-3">
+                    <NavLink
+                      className="nav-item"
+                      to="/profile"
+                      style={{ textDecoration: "none" }}
+                    >
+                      <p className="mb-0 text-light">
+                        Hi, {data.nama || "User"}
+                      </p>
+                    </NavLink>
+                    <NavLink
+                      className="nav-item"
+                      to="/wallet"
+                      style={{ textDecoration: "none" }}
+                    >
+                      <p className="mb-0 text-light">
+                        Saldo: {Rupiah.format(parseInt(data.wallet))}
+                      </p>
+                    </NavLink>
+                  </div>
+                  <NavLink
+                    type="button"
+                    className="btn btn-outline-light ms-3 me-3"
+                    to="/sell"
+                    style={{ minWidth: "80px" }}
+                  >
+                    JUAL
+                  </NavLink>
+                  {/* <NavLink type="button" className="btn btn-primary" to="/profile">Profile</NavLink> */}
+                  <NavLink
+                    type="button"
+                    className="btn btn-outline-light"
+                    to="/logout"
+                    style={{ minWidth: "80px" }}
+                    onClick={() => handleClick()}
+                  >
+                    LOGOUT
+                  </NavLink>
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+      </nav>
+      <Outlet />
+      <Footer />
+    </>
+  );
 }
