912d0c0ca72c6137a32616feed7d9c7e97f6ef1a
gaby <76735916+gabypaulina@users.noreply.github.com>
2023-12-12 23:43:16 +0700

    update

--- ditawarin/src/WalletPage.jsx
+++ ditawarin/src/WalletPage.jsx
@@ -9,6 +9,7 @@ export default function WalletPage(){
 	let userToken = localStorage.getItem("token");
     const wallet_data = useLoaderData();
     const [tarik, setTarik] = useState(false)
+    const [top, setTop] = useState(false);
 
 	const {
 		register,
@@ -56,6 +57,10 @@ export default function WalletPage(){
 		setTarik(true);
 	};
 
+    const topClick = () => {
+        setTop(true)
+    }
+
 
     return (
         <>
@@ -65,16 +70,30 @@ export default function WalletPage(){
             <div className="container">
                 <div className="row mt-5 justify-content-between">
                     <h1 className="mb-4" style={{color: "#06083D"}}><b>Detail Saldo</b></h1>
-                    {!tarik && (
                     <div className="col-5 border border-secondary-subtle p-4" style={{borderRadius: "45px"}}>
-                        <div className="d-flex mb-4 pb-3 align-items-center border-bottom">
-                            <img src={uang} alt="" style={{width: "40px", height: "40px"}}/>
-                            <div className="ketSaldo ms-3">
-                                <p className="mb-0">Total Saldo Aktif</p>
-                                <p className="mb-0" style={{fontWeight:"bold", color: "#06083D", fontSize:"22px"}}>{Rupiah.format(wallet_data.wallet.result.saldo)}</p>
+                        {!top && (
+                            <div className="d-flex mb-4 pb-3 align-items-center border-bottom">
+                                <img src={uang} alt="" style={{width: "40px", height: "40px"}}/>
+                                <div className="ketSaldo ms-3">
+                                    <p className="mb-0">Total Saldo Aktif</p>
+                                    <p className="mb-0" style={{fontWeight:"bold", color: "#06083D", fontSize:"22px"}}>{Rupiah.format(wallet_data.wallet.result.saldo)}</p>
+                                </div>
+                                <button type="button" className="btn btn-success ms-auto" onClick={()=> {topClick()}}><b>Topup Saldo</b></button>  
                             </div>
-                            <button type="button" className="btn btn-success ms-auto" onClick={()=> {tarikClick()}}><b>Tarik Saldo</b></button>
-                        </div>
+                        )}
+                        {top && (
+                            <div className="d-flex mb-4 pb-3 align-items-center border-bottom">
+                                <img src={uang} alt="" style={{width: "40px", height: "40px"}}/>
+                                <div className="ketSaldo ms-3">
+                                    <p className="mb-0">Total Saldo Aktif</p>
+                                    <p className="mb-0" style={{fontWeight:"bold", color: "#06083D", fontSize:"22px"}}>{Rupiah.format(wallet_data.wallet.result.saldo)}</p>
+                                </div>
+                                <div className="topup ms-auto text-end">
+                                    <input type="text"placeholder="Jumlah topup" className=" ms-auto ps-3 border border-secondary-subtle" style={{borderRadius: "10px", width: "13rem", height: "3rem"}}/> <br />
+                                    <button type="button" className="btn btn-success ms-auto mt-2"><b>Topup Saldo</b></button>
+                                </div>
+                            </div>
+                        )}
                         <div className="d-flex">
                             <p className="mb-0">Saldo Refund :</p>
                             <p className="ms-auto mb-0">{Rupiah.format(wallet_data.wallet.result.saldo_tertahan)}</p>
@@ -83,31 +102,17 @@ export default function WalletPage(){
                             <p className="mb-0">Saldo Tertahan :</p>
                             <p className="ms-auto mb-0">Rp 0</p>
                         </div>
-                    </div>
-                    )}
-                    {tarik && (
-                    <div className="col-5 border border-secondary-subtle p-4" style={{borderRadius: "45px"}}>
-                        <div className="d-flex mb-4 pb-3 align-items-center border-bottom">
-                            <img src={uang} alt="" style={{width: "40px", height: "40px"}}/>
-                            <div className="ketSaldo ms-3">
-                                <p className="mb-0">Total Saldo Aktif</p>
-                                <p className="mb-0" style={{fontWeight:"bold", color: "#06083D", fontSize:"22px"}}>{Rupiah.format(wallet_data.wallet.result.saldo)}</p>
+                        {!tarik && (
+                            <button type="button" className="btn btn-success mt-2" style={{float: "right"}} onClick={()=> {tarikClick()}}><b>Tarik Saldo</b></button>
+                        )}
+                        {tarik && (
+                            <div className="d-flex mt-2" style={{float: "right"}}>
+                                <input type="text"placeholder="Jumlah yang ingin ditarik" className="me-2 ps-3 border border-secondary-subtle" style={{borderRadius: "10px", width: "13rem", height: "3rem"}}/> <br />
+                                <button type="button" className="btn btn-success"><b>Tarik Saldo</b></button>
                             </div>
-                            <div className="tarikSaldo ms-auto text-end">
-                                <input type="text"placeholder="Jumlah yang ingin ditarik" className=" ms-auto ps-3 border border-secondary-subtle" style={{borderRadius: "10px", width: "13rem", height: "3rem"}}/> <br />
-                                <button type="button" className="btn btn-success mt-2"><b>Tarik Saldo</b></button>
-                            </div>
-                        </div>
-                        <div className="d-flex">
-                            <p className="mb-0">Saldo Refund :</p>
-                            <p className="ms-auto mb-0">{Rupiah.format(wallet_data.wallet.result.saldo_tertahan)}</p>
-                        </div>
-                        <div className="d-flex">
-                            <p className="mb-0">Saldo Tertahan :</p>
-                            <p className="ms-auto mb-0">Rp 0</p>
-                        </div>
+                        )}
+                        
                     </div>
-                    )}
 
                     <div className="col-6 border border-secondary-subtle p-4" style={{borderRadius: "45px"}}>
                         <h2 className="mb-0" style={{color: "#06083D"}}><b>Riwayat Saldo</b></h2>
