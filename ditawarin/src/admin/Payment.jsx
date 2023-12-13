8dbc79a4700bd4b9fb67c972d8c8963f86e5570f
Hendra <hendra.i21@mhs.istts.ac.id>
2023-12-13 21:59:32 +0700

    get all purchase admin

--- ditawarin/src/admin/Payment.jsx
+++ ditawarin/src/admin/Payment.jsx
@@ -3,34 +3,52 @@ import Edit from "../assets/edit.png";
 import Check from "../assets/check.png";
 import Eye from "../assets/eye.png";
 import BarChart from "../components/BarChart";
+import { useLoaderData } from "react-router-dom";
+import { useEffect } from "react";
 
 function Payment() {
+	const data = useLoaderData();
+
+	const sortPurchase = () => {
+		data.sort((a, b) => {
+			return b.transaction - a.transaction;
+		});
+	}
+
+	useEffect(() => {
+		sortPurchase();
+	}, []);
+	
 	return (
 		<>
+		{console.log(data)}
 			<table className="table">
 				<thead>
 					<tr className="table-success">
 						<th>PRODUCT</th>
 						<th>PRICE</th>
 						<th>STATUS</th>
-						<th>RATING</th>
 						<th>ACTION</th>
 					</tr>
 				</thead>
 				<tbody>
-					<tr>
-						<td></td>
-						<td>Rp. </td>
-						<td style={{ width: "100px" }}>
-							<div className="bg-success rounded text-center p-1">DONE</div>
-						</td>
-						<td>⭐⭐⭐⭐⭐</td>
-						<td className="w-20">
-							<button className="bg-secondary rounded border-0">
-								<img src={Eye} style={{ width: "35px" }} />
-							</button>
-						</td>
-					</tr>
+					{data.map((a) => {
+						let url = import.meta.env.VITE_API_URL + "/static/" + a.item;
+						return (
+							<tr>
+								<td><img src={url} style={{width : "100px"}}/></td>
+								<td>Rp. {a.transaction}</td>
+								<td style={{ width: "100px" }}>
+									<div className="bg-success rounded text-center p-1">DONE</div>
+								</td>
+								<td className="w-20">
+									<button className="bg-secondary rounded border-0">
+										<img src={Eye} style={{ width: "35px" }} />
+									</button>
+								</td>
+							</tr>
+						);
+					})}
 				</tbody>
 			</table>
 			<BarChart />
